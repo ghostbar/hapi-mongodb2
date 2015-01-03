4f60f7b80c1d6c8f236c57a646130fef2f1cc6cf
Jose Luis Rivas <me@ghostbar.co>
2015-01-03 00:12:10 -0500

    Main lib functionality

---
+++ index.js
@@ -0,0 +1,25 @@
+'use strict';
+
+var mongodb = require('mongodb').MongoClient;
+
+exports.register = function (plugin, opts, next) {
+  opts = opts || {};
+  opts.url = opts.url || 'mongodb://localhost:27017/test';
+
+  mongodb.connect(opts.url, function (err, db) {
+    if (err) {
+      plugin.log(['hapi-mongodb2', 'error'], 'Error connection to MongoDB database');
+      console.error(err);
+      return next(err);
+    }
+
+    plugin.log(['hapi-mongodb2', 'info'], 'Successfully connected to MongoDB datastore');
+    plugin.expose('client', db);
+    plugin.bind({ mongo: db });
+    next();
+  });
+};
+
+exports.register.attributes = {
+  pkg: require('./package.json')
+};
